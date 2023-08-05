193df02071caf53fd68480ab0dfd7283fa31f0ed
shravan77r <136837547+shravan77r@users.noreply.github.com>
2023-08-05 10:28:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArticles(name) {
+  return name.replace(/^(the|a|an)\s+/i, "").trim();
+}
+
+touristSpots.sort((a, b) => {
+  const nameA = removeArticles(a.toLowerCase());
+  const nameB = removeArticles(b.toLowerCase());
+  return nameA.localeCompare(nameB);
+});
+
+function displayBands(bands) {
+  const ul = document.getElementById("band");
+  ul.innerHTML = ''; // Clear existing content
+
+  for (const band of bands) {
+    const li = document.createElement("li");
+    li.textContent = band;
+    ul.appendChild(li);
+  }
+}
+displayBands(touristSpots);
